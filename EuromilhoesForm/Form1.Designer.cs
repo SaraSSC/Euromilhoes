e5b0d0053d067eba20e4daeba35a7e5f83773a8c
Sattineez_SC <83277661+SaraSSC@users.noreply.github.com>
2024-05-21 20:57:38 +0100

    Code and background image

---
+++ EuromilhoesForm/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace EuromilhoesForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lblResultChaves = new System.Windows.Forms.Label();
+            this.cBLChave = new System.Windows.Forms.CheckedListBox();
+            this.cLBEstrela = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblChaves = new System.Windows.Forms.Label();
+            this.btnGerarChave = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblResultChaves
+            // 
+            this.lblResultChaves.AutoSize = true;
+            this.lblResultChaves.BackColor = System.Drawing.Color.RoyalBlue;
+            this.lblResultChaves.Font = new System.Drawing.Font("Perpetua Titling MT", 16.2F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultChaves.ForeColor = System.Drawing.Color.Yellow;
+            this.lblResultChaves.Location = new System.Drawing.Point(22, 20);
+            this.lblResultChaves.Name = "lblResultChaves";
+            this.lblResultChaves.Size = new System.Drawing.Size(231, 33);
+            this.lblResultChaves.TabIndex = 0;
+            this.lblResultChaves.Text = "Euromilhões";
+            this.lblResultChaves.Click += new System.EventHandler(this.lblResultChaves_Click);
+            // 
+            // cBLChave
+            // 
+            this.cBLChave.FormattingEnabled = true;
+            this.cBLChave.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18",
+            "19",
+            "20",
+            "21",
+            "22",
+            "23",
+            "24",
+            "25",
+            "26",
+            "27",
+            "28",
+            "29",
+            "30",
+            "31",
+            "32",
+            "33",
+            "34",
+            "35",
+            "36",
+            "37",
+            "38",
+            "39",
+            "40",
+            "41",
+            "42",
+            "43",
+            "44",
+            "45",
+            "46",
+            "47",
+            "48",
+            "49",
+            "50"});
+            this.cBLChave.Location = new System.Drawing.Point(12, 195);
+            this.cBLChave.MultiColumn = true;
+            this.cBLChave.Name = "cBLChave";
+            this.cBLChave.Size = new System.Drawing.Size(664, 225);
+            this.cBLChave.TabIndex = 1;
+            this.cBLChave.UseCompatibleTextRendering = true;
+            this.cBLChave.SelectedIndexChanged += new System.EventHandler(this.cBLChave_SelectedIndexChanged);
+            // 
+            // cLBEstrela
+            // 
+            this.cLBEstrela.FormattingEnabled = true;
+            this.cLBEstrela.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9"});
+            this.cLBEstrela.Location = new System.Drawing.Point(699, 195);
+            this.cLBEstrela.Name = "cLBEstrela";
+            this.cLBEstrela.Size = new System.Drawing.Size(89, 208);
+            this.cLBEstrela.TabIndex = 2;
+            this.cLBEstrela.SelectedIndexChanged += new System.EventHandler(this.cLBEstrela_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Lavender;
+            this.label1.Font = new System.Drawing.Font("Palatino Linotype", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(712, 155);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 24);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Estrelas";
+            // 
+            // lblChaves
+            // 
+            this.lblChaves.AutoSize = true;
+            this.lblChaves.BackColor = System.Drawing.Color.Lavender;
+            this.lblChaves.Font = new System.Drawing.Font("Palatino Linotype", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblChaves.Location = new System.Drawing.Point(276, 152);
+            this.lblChaves.Name = "lblChaves";
+            this.lblChaves.Size = new System.Drawing.Size(78, 27);
+            this.lblChaves.TabIndex = 5;
+            this.lblChaves.Text = "Chaves";
+            this.lblChaves.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnGerarChave
+            // 
+            this.btnGerarChave.Location = new System.Drawing.Point(442, 85);
+            this.btnGerarChave.Name = "btnGerarChave";
+            this.btnGerarChave.Size = new System.Drawing.Size(186, 37);
+            this.btnGerarChave.TabIndex = 7;
+            this.btnGerarChave.Text = "Gerar chave";
+            this.btnGerarChave.UseVisualStyleBackColor = true;
+            this.btnGerarChave.Click += new System.EventHandler(this.btnGerarChave_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(863, 450);
+            this.Controls.Add(this.btnGerarChave);
+            this.Controls.Add(this.lblChaves);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cLBEstrela);
+            this.Controls.Add(this.cBLChave);
+            this.Controls.Add(this.lblResultChaves);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblResultChaves;
+        private System.Windows.Forms.CheckedListBox cBLChave;
+        private System.Windows.Forms.CheckedListBox cLBEstrela;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblChaves;
+        private System.Windows.Forms.Button btnGerarChave;
+    }
+}
+
---
+++ EuromilhoesForm/Form1.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EuromilhoesForm
+{
+    public partial class Form1 : Form
+
+    {
+           
+        public Form1()
+        {
+            InitializeComponent();
+
+            cBLChave.ItemCheck += (s, e) =>
+            {
+                if (cBLChave.CheckedItems.Count >= 5 && e.NewValue == CheckState.Checked)
+                {
+                    e.NewValue = CheckState.Unchecked;
+                }
+            };
+
+            cLBEstrela.ItemCheck += (s, e) =>
+            {
+                if (cLBEstrela.CheckedItems.Count >= 2 && e.NewValue == CheckState.Checked)
+                {
+                    e.NewValue = CheckState.Unchecked;
+                }
+            };
+        }
+        private void GerarChaveEComparar()
+        {
+            Random rand = new Random();
+            List<int> chaveNumeros = new List<int>();
+            List<int> chaveEstrelas = new List<int>();
+
+            // Gerar 5 números únicos para a chave
+            while (chaveNumeros.Count < 5)
+            {
+                int num = rand.Next(1, 51); // números de 1 a 50
+                if (!chaveNumeros.Contains(num))
+                {
+                    chaveNumeros.Add(num);
+                }
+            }
+
+            // Gerar 2 estrelas únicas para a chave
+            while (chaveEstrelas.Count < 2)
+            {
+                int estrela = rand.Next(1, 10); // estrelas de 1 a 9
+                if (!chaveEstrelas.Contains(estrela))
+                {
+                    chaveEstrelas.Add(estrela);
+                }
+            }
+
+            // Obter a aposta do usuário
+            
+            List<int> apostaNumeros = cBLChave.CheckedItems.Cast<string>().Select(int.Parse).ToList();
+            List<int> apostaEstrelas = cLBEstrela.CheckedItems.Cast<string>().Select(int.Parse).ToList();
+
+
+            // Comparar a aposta com a chave e determinar o prêmio
+            int acertosNumeros = apostaNumeros.Intersect(chaveNumeros).Count();
+            int acertosEstrelas = apostaEstrelas.Intersect(chaveEstrelas).Count();
+
+            string premio;
+            if (acertosNumeros == 5 && acertosEstrelas == 2)
+            {
+                premio = "1º Prêmio";
+            }
+            else if (acertosNumeros == 5 && acertosEstrelas == 1)
+            {
+                premio = "2º Prêmio";
+            }
+            else if (acertosNumeros == 5)
+            {
+                premio = "3º Prêmio";
+            }
+            else if (acertosNumeros == 4 && acertosEstrelas == 2)
+            {
+                premio = "4º Prêmio";
+            }
+            else if (acertosNumeros == 4 && acertosEstrelas == 1)
+            {
+                premio = "5º Prêmio";
+            }
+            else if (acertosNumeros == 4)
+            {
+                premio = "6º Prêmio";
+            }
+            else
+            {
+                premio = "Sem prêmio";
+            }
+
+            // Exibir o resultado
+            
+            MessageBox.Show( $"Você acertou {acertosNumeros} números\n e {acertosEstrelas} estrelas.\n {premio}!\n");
+            
+
+            // Exibir as chaves e as estrelas que foram geradas
+            string chaveNumerosStr = string.Join(", ", chaveNumeros);
+            string chaveEstrelasStr = string.Join(", ", chaveEstrelas);
+            lblResultChaves.Text = $"Chaves geradas: {chaveNumerosStr}| Estrelas: {chaveEstrelasStr} ";
+        }
+
+        private void btnGerarChave_Click(object sender, EventArgs e)
+        {
+            // Verificar se o usuário selecionou 5 números e 2 estrelas
+            if (cBLChave.CheckedItems.Count != 5)
+            {
+                MessageBox.Show("Por favor, selecione exatamente 5 números.");
+                return;
+            }
+            if (cLBEstrela.CheckedItems.Count != 2)
+            {
+                MessageBox.Show("Por favor, selecione exatamente 2 estrelas.");
+                return;
+            }
+
+            GerarChaveEComparar();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblResultChaves_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblResultEstrelas_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cBLChave_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void cLBEstrela_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGerarChave_Click_1(object sender, EventArgs e)
+        {
+            GerarChaveEComparar();
+        }
+    }
+}
